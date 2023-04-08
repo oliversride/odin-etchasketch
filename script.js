0b36a8a83500813a1f2e331421010d73e324ebc9
Richard Hoffman <oliversride@gmail.com>
2023-04-07 18:33:23 -0700

    Add button to let user enter size

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ setupGrid(16);
 function setupGrid(gridSize) {
   // Calculate minimum dimension of viewport
   const minDimension = Math.min(window.innerWidth, window.innerHeight);
-  const size = `${minDimension/1.5}px`;
+  const size = `${minDimension / 1.5}px`;
 
   // Set width and height of gridContainer to be equal
   const gridContainer = document.querySelector('.gridContainer');
@@ -14,9 +14,17 @@ function setupGrid(gridSize) {
   gridContainer.style.top = '50%';
   gridContainer.style.left = '50%';
   gridContainer.style.transform = 'translate(-50%, -50%)';
-  gridContainer.style['background'] = 'green';
   gridContainer.style['display'] = 'flex';
   gridContainer.style['flex-direction'] = 'column';
+
+  // Make the button look good
+  const sizer = document.querySelector('.sizer');
+  sizer.style['width'] = size;
+  sizer.style['display'] = 'block';
+  sizer.style['margin'] = '0px auto 10px';
+  sizer.style['padding'] = '10px';
+  sizer.style['font-size'] = '32px';
+  sizer.style['font-weight'] = '800';
 
   // Make gridSize x gridSize grid
   for (let i = 0; i < gridSize; i++) {
@@ -41,3 +49,14 @@ function setupGrid(gridSize) {
     gridContainer.appendChild(rowDiv);
   }
 }
+
+const sizer = document.querySelector('.sizer');
+sizer.addEventListener('click', () => {
+  const answer = prompt('Enter N (max 100):', '16');
+  console.log('answer=' + answer);
+  let n = Number(answer);
+  if (n < 0) n = 1;
+  if (n > 100) n = 100;
+  if (isNaN(n)) n = 16;
+  console.log('n=' + n);
+});
